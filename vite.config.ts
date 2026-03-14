0dcc4627dd8fcb66c83ea3d0b0cd1c0ab519d96b
farismash28 <70283271+farismash28@users.noreply.github.com>
2026-03-14 01:55:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/My-Resume/' : '/',
+  base: command === 'build' ? '/farismash28.github.io/' : '/',
   plugins: [
     tailwindcss(),
     react(),
